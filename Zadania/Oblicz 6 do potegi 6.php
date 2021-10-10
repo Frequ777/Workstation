c66339342e3a13ea31a8ba3baeb89caae28fa515
Michał Jan Lewandowski <91124839+Frequ777@users.noreply.github.com>
2021-10-10 12:38:40 +0200

    Add files via upload

---
+++ Zadania/Oblicz 6 do potegi 6.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Liczba a podzielna przez 2 i przez 3</title>
+    <meta charset=utf-8>
+    <meta name="author" content="Michał Jan Lewandowski">
+    </head>
+<body>
+
+    <?php  
+    
+    echo "Napisz program, który obliczy 6 do potęgi 6.",'<br>';
+    $a=6;
+    echo "Wynik potęgi wynosi: ", ($a**$a), '<br>';
+    
+    
+    ?>
+</body>
+
+</html>
